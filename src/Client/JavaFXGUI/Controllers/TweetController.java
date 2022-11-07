c86943b50a47ca89c05c3145b077db09c2c0b5f2
Shvji <aponte2001@gmail.com>
2022-11-08 00:29:04 +0100

    added tweet controller

---
+++ src/Client/JavaFXGUI/Controllers/TweetController.java
@@ -0,0 +1,22 @@
+package Client.JavaFXGUI.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TweetController {
+
+    @FXML
+    private Label dateField;
+
+    @FXML
+    private Label hashtagField;
+
+    @FXML
+    private Label messageField;
+
+    @FXML
+    private Label usernameField;
+
+    public void initialize() {
+    }
+}
